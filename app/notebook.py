c76e1267641df7f3be61f901bac50ce83a0222e1
carzap <carvajalzapatamateo@gmail.com>
2025-02-18 12:54:13 -0500

    atributos

--- app/notebook.py
+++ app/notebook.py
@@ -1,5 +1,19 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+
+from datetime import datetime
+
 class Note:
     HIGH = "HIGH"
     MEDIUM = "MEDIUM"
     LOW = "LOW"
+
+    def __init__(self, code: str, title: str, text: str, importance: str):
+        self.code: str = code
+
+        self.title: str = title
+
+        self.text: str = text
+
+        self.importance: str = importance
+
+
